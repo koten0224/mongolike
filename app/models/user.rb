63ed39e63547148d655507bb099d7ef29d0ac4b4
Koten <koten0224@gmail.com>
2020-09-03 01:24:53 +0800

    user password autofill complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+  after_initialize :auto_fill_password, if: ->{Rails.env.development?}
+  def auto_fill_password
+    self.password = '111111'
+    self.password_confirmation = '111111'
+  end
 end
